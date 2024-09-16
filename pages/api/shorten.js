099aa9a5ad64a32aecb0d6d5ed32bf171541945b
Khushaalan <khushaalan@veecotech.com>
2024-09-17 01:51:42 +0800

    fix:remove redundant code

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -1,39 +1,13 @@
-const urlMapsArray = [new Map()];
-
 export default function handler(req, res) {
   if (req.method === "POST") {
     const { url } = req.body;
-    let found = false;
-    let shortUrlId;
-
-    for (let map of urlMapsArray) {
-      if (map.has(url)) {
-        found = true;
-        shortUrlId = map.get(url);
-        break;
-      }
-    }
-
-    if (found) {
-      return res.status(200).json({
-        shortUrl: `${req.headers["x-forwarded-proto"] || "http"}://${
-          req.headers.host
-        }/${shortUrlId}`,
-        message: "We found the shortened URL in our record.",
-      });
-    }
-
     const newId = Math.random().toString(36).substring(2, 10);
     const newMap = new Map();
     newMap.set(url, newId);
 
-    urlMapsArray.push(newMap);
-
     return res.status(200).json({
-      shortUrl: `${req.headers["x-forwarded-proto"] || "http"}://${
-        req.headers.host
-      }/${newId}`,
-      message: "Created a new record for the shortened URL.",
+      url_id: newId,
+      url: url,
     });
   } else {
     res.setHeader("Allow", ["POST"]);
