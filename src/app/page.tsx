17d31f64fcd54be48129c6b9b969942b49f851c5
Khushaalan <khushaalan@veecotech.com>
2024-09-16 21:38:47 +0800

    feat: implement magic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
+
+import { cn } from "@/lib/utils";
+import DotPattern from "@/components/magicui/dot-pattern";
+import ShimmerButton from "@/components/magicui/shimmer-button";
+import ShinyButton from "@/components/magicui/shiny-button";
 import { useState } from "react";
 
-export default function Home() {
+export default function DotPatternDemo() {
   const [url, setUrl] = useState("");
   const [shortenedUrl, setShortenedUrl] = useState("");
+  const [message, setMessage] = useState("");
 
   async function shortenUrl() {
     const response = await fetch("/api/shorten", {
@@ -16,27 +22,53 @@ export default function Home() {
     const data = await response.json();
     console.log(data);
     setShortenedUrl(data.shortUrl);
+    setMessage(data.message);
   }
 
+  async function copyUrl() {
+    await console.log("copying");
+    await navigator.clipboard.writeText(shortenedUrl);
+  }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <div>
-        <div>
-          <input
-            type="text"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            className="p-2 border border-gray-300 rounded-md text-black"
-          />
-          <button
-            className="p-2 bg-blue-500 text-white rounded-md"
-            onClick={shortenUrl}
-          >
-            Shorten URL
-          </button>
-        </div>
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden">
+      <DotPattern
+        className={cn(
+          "absolute inset-0 w-full h-full [mask-image:radial-gradient(300px_circle_at_center,white,transparent)]"
+        )}
+      />
+      <div className="relative z-10">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <p className="text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
+            CutX - URL Shortener
+          </p>
 
-        <h1>Shortened URL :{shortenedUrl}</h1>
+          <div className="flex flex-col items-center justify-center py-2">
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <div className="flex items-center justify-center space-x-2">
+                <input
+                  type="text"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                  className="p-2 border border-gray-300 rounded-md text-black"
+                />
+
+                <ShimmerButton className="shadow-2xl" onClick={shortenUrl}>
+                  <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                    Shorten
+                  </span>
+                </ShimmerButton>
+              </div>
+              {shortenedUrl && message && (
+                <>
+                  <p>{message}</p>
+                  <div onClick={copyUrl}>
+                    <ShinyButton text="Copy URL" className="shadow-2xl" />
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
